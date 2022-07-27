6d178a9bedf602eacfeb9b6f19db6947e90719ca
Exoalam <nafiul.arpon@gmail.com>
2022-07-27 22:12:08 +0600

    Final Change

--- app/src/test/java/com/example/cardiacrecorder/finalUnitTest.java
+++ app/src/test/java/com/example/cardiacrecorder/finalUnitTest.java
@@ -9,37 +9,9 @@ import org.robolectric.RuntimeEnvironment;
 
 @RunWith(RobolectricTestRunner.class)
 public class finalUnitTest {
-    @Test
-    public void addDataTest()
-    {
-        dbmanager dbmanager=new dbmanager(RuntimeEnvironment.application);
-        String hr,s,d,c,dt;
-        hr="123";s="60";d="80";
-        c="This is not good";
-        dt=""+System.currentTimeMillis();
-        long dataid=dbmanager.addData(hr,s,d,c,dt);
-        assertTrue(dataid>0);
-        dbmanager.close();
-    }
-
-
-
-    @Test
-    public void testDelete()
-    {
-        dbmanager dbmanager=new dbmanager(RuntimeEnvironment.application);
-
-        String hr,s,d,c,dt;
-        hr="123";s="60";d="80";
-        c="This is not good";
-        dt=""+System.currentTimeMillis();
-        long dataid=dbmanager.addData(hr,s,d,c,dt);
-        int intdataid=(int)dataid;
-        long p=dbmanager.deleteData(intdataid);
-        assertTrue(p>0);
-        dbmanager.close();
-    }
-
+    /**\
+     * data update test
+     */
     @Test
     public void testUpdate()
     {
